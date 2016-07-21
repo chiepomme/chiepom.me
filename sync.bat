0f274d6cb1b8dbc183953f23fb13bfa5f06a52d3
chiepomme <chie@loves-miko.cc>
2016-07-22 01:35:07 +0900

    updatesync

--- sync.bat
+++ sync.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
-chienote sync && chienote convert && git add -A && git commit -m "update" && git push origin master
+chienote sync && chienote convert && git add -A && git commit -m "update" && git push origin gh-pages
 pause
